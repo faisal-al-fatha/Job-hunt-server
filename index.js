eee84f720074d99700a0e92f05d81d95fa610f84
faisal-al-fatha <faysal313@gmail.com>
2023-02-18 15:02:31 +0600

    api created

--- index.js
+++ index.js
@@ -15,6 +15,30 @@ app.get('/job-data', (req, res) => {
     res.send(data)
 })
 
+app.get('/fresher-job/:id', (req, res) => {
+    const id = req.params.id;
+    const job = data.fresherJobs.find(j => j.id === id);
+    res.send(job)
+})
+
+app.get('/senior-job/:id', (req, res) => {
+    const id = req.params.id;
+    const job = data.SeniorJobs.find(j => j.id === id);
+    res.send(job)
+})
+
+app.get('/world-topIt/:id', (req, res) => {
+    const id = req.params.id;
+    const company = data.worldTopItCompanies.find(c => c.id === id);
+    res.send(company)
+})
+
+app.get('/bd-topIt/:id', (req, res) => {
+    const id = req.params.id;
+    const company = data.bdTopItCompanies.find(c => c.id === id);
+    res.send(company)
+})
+
 app.listen(port, ()=> {
     console.log('Job Hunt server running on port', port);
 })
